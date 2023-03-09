003974bc7d69e2d617d2ea1022408146f45db218
Jeanette Williamson <JeanetteWilliamson10@yahoo.com>
2023-03-09 10:41:07 -0500

    added front end url

--- app.js
+++ app.js
@@ -21,14 +21,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(
-    cors()
-  );
   // app.use(
   //   cors({
   //     origin: [process.env.FRONTEND_URI]  // <== URL of our future React app
   //   })
   // );
+  app.use(
+    cors({
+      origin: [process.env.FRONTEND_URI]  // <== URL of our future React app
+    })
+  );
 
 
 app.use('/parent', parentRouter);
